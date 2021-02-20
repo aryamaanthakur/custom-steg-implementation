7842b5b6c6e19ed5df81e6a002e9f89914348b98
aryamaanthakur <aryamaan.thakur@outlook.com>
2021-02-20 16:27:13 +0530

    Created a more general encoding function

--- lsb_steg.py
+++ lsb_steg.py
@@ -1,6 +1,5 @@
 import numpy as np
 from PIL import Image as im
-import math
 from itertools import permutations
 
 def bin_to_ascii(bin_msg):
@@ -10,52 +9,66 @@ def bin_to_ascii(bin_msg):
 
     return ascii_msg
 
-def hide(img, msg, msg_end="#steg-end#"):
-    width, height = img.size
-    msg+=msg_end #using a string to determine the end of message while decoding
-    msg_bin = "".join([f'{ord(i):08b}' for i in msg]) #converting ascii message to 8bit binary form
-    msg_len = len(msg_bin)
-    img_array = np.array(list(img.getdata())) #image as a continuous array of pixels
-    if img.mode == "RGB": n = 3
-    elif img.mode == "RGBA": n = 4
-    else:
-        print("Invalid image mode")
-        return None
+def ascii_to_bin(ascii_msg):
+    bin_msg = ""
+    for i in ascii_msg:
+        bin_msg+=f'{ord(i):08b}'
 
-    max_lsb = width*height*n
+    return bin_msg
 
-    #checking if image has sufficient pixels to hide data
-    if (msg_len > max_lsb):
-        print("Image not large enough to hide data")
+def hide(img, img_arr, channel_order, bit_start, bit_end, msg):
+
+    msg = ascii_to_bin(msg)
+    msg_len = len(msg)
+
+    height, width, z = img_arr.shape
+
+    max_msg_len = height*width*len(channel_order)*(bit_end-bit_start)
+
+    if max_msg_len < msg_len:
+        print("[-] Image not large enough to hide message in given channel order")
         return None
     
+    bit_len = bit_end-bit_start
     index = 0
-    for i in range(msg_len):
-        for j in range(n):
-            img_array[i][j] = int(bin(img_array[i][j])[2:-1] + msg_bin[index], 2)
-            index+=1
-        if index==msg_len: break
+    for x in range(height):
+        for y in range(width):
+            for z in channel_order:
+                curr_value = f'{img_arr[x,y,z]:08b}'
+                if bit_end==8:
+                    new_value = curr_value[0:bit_start]+msg[index:index+bit_len]
+                else:
+                    new_value = curr_value[0:bit_start]+msg[index:index+bit_len]+curr_value[bit_end:]
 
-    img_array=img_array.reshape(height, width, n)
-    new_img = im.fromarray(img_array.astype('uint8'), img.mode)
-    print("Image Encoded Successfully")
+                img_arr[x,y,z] = int(new_value,2)
+                index+=bit_len
+                
+                if index>=msg_len:
+                    new_img = im.fromarray(img_arr.astype('uint8'), img.mode)
+                    print("Image Encoded Successfully")
+                    return new_img
+
     return new_img
+    
 
 img = im.open("test/test.png", "r")
-new_img = hide(img, r"gunnHacks{abcdefgasdmfalsjdflaskjdflkdf}")
+img_arr = np.array(img)
+new_img = hide(img, img_arr, [2,3,1,0], 6, 8, "Hello hello hello, this is cyberlabs! What up?")
 new_img.save("test/test0.png")
 
 
 def extract(img_arr, channel_order, bit_start, bit_end, msg_len):
     hidden_msg = ""
     height, width, z = img_arr.shape
+    bit_len=bit_end-bit_start
     c = 0
     for x in range(height):
         for y in range(width):
             for z in channel_order:
                 hidden_msg += f'{img_arr[x,y,z]:08b}'[bit_start:bit_end]
-                c+=1
-                if c==msg_len: return hidden_msg
+                c+=bit_len
+                if c>=msg_len: return hidden_msg
+
     return hidden_msg
 
 def analyse(img, bit_start, bit_end, msg_len=400, search=""):
@@ -74,8 +87,13 @@ def analyse(img, bit_start, bit_end, msg_len=400, search=""):
         print("Invalid Image Mode")
         return None
 
-    if bit_end-bit_start==1: print("[=] Checking bit number", bit_start+1, "\n")
-    else: print("[=] Checking from bit", bit_start+1, "to", bit_end, "\n")
+    if bit_end-bit_start==1:
+        print("[=] Checking bit number", bit_start+1, "\n")
+    else:
+        print("[=] Checking from bit", bit_start+1, "to", bit_end, "\n")
+
+    if search!="":
+        print("[=] Searching for keyword", search)
 
     for test in channel_orders:
         channel_order = [img.mode.index(channel) for channel in test]
@@ -86,7 +104,7 @@ def analyse(img, bit_start, bit_end, msg_len=400, search=""):
 
 img = im.open("test/test0.png", "r")
 img_arr = np.array(img)
-bin_msg = extract(img_arr, [0,1,2,3], 7, 8, 500)
+#bin_msg = extract(img_arr, [0,1,2], 7, 8, 500)
 #print(bin_to_ascii(bin_msg))
 
-analyse(img, 7, 8)
+analyse(img, 6, 8, 400)
