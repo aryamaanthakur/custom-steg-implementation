21c6fcf086080e33279704219c1ea9cb367eaec7
aryamaanthakur <aryamaan.thakur@outlook.com>
2021-02-26 22:15:27 +0530

    Used multiprocessing to make the program faster

--- bit_plane_slicing.py
+++ bit_plane_slicing.py
@@ -1,21 +1,43 @@
 import numpy as np
 from PIL import Image as im
+import multiprocessing 
+import time
 
 def slice(img_arr, channel, bit_index):
     height, width, z = img_arr.shape
 
     new_image = im.new('1', (width, height))
     new_image_data = new_image.load()
-
+    
     for x in range(height):
         for y in range(width):
-            new_image_data[y,x]=int(f'{img_arr[x,y,channel]:08b}'[bit_index])*255
+            try:
+                new_image_data[y,x]=int(f'{img_arr[x,y,channel]:08b}'[bit_index])*255
+            except IndexError:
+                pass
 
-    return new_image
-img = im.open(r"test/bitplane.png", "r")
-img_arr = np.array(img)
+    new_image.save(r"test/bitplane/{}{}.png".format("RGBA"[channel], bit_index))
+    print("[+] {}{} Done!".format("RGBA"[channel], bit_index))
 
-for channel in range(4):
-    for bit_index in range(8):
-        new_image = slice(img_arr, channel, bit_index)
-        new_image.save(r"test/bitplane/{}{}.png".format("RGBA"[channel], bit_index))
+if __name__ == "__main__": 
+    img = im.open(r"test/bitplane.png", "r")
+    img_arr = np.array(img)
+
+    print(img_arr.shape)
+    t1 = time.time()
+    if img.mode == "RGBA": n=4
+    elif img.mode == "RGB": n=3
+
+
+    for channel in range(n):
+        threads = []
+        for bit_index in range(8):
+            threads.append(multiprocessing.Process(target=slice, args=(img_arr, channel, bit_index)))
+        for thread in threads:
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+            
+    t2 = time.time()
+    print("Time taken for execution with threading", t2-t1, "seconds")
