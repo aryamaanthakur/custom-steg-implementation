e064a6884bae6998d57986a65d67ff44c847b6c0
aryamaanthakur <aryamaan.thakur@outlook.com>
2021-04-09 10:22:20 +0530

    Solved relative path issue for linux

--- csi.py
+++ csi.py
@@ -9,27 +9,24 @@ parser.add_argument('command', choices = ['bph', 'bps', 'lsbh', 'lsbu', 'triplea
 args = parser.parse_args()
 
 if args.command=="bps":
-    with open("bitplane\\bit_plane_slicing_gui.py", "rb") as source_file:
-        code = compile(source_file.read(), "bitplane\\bit_plane_slicing_gui.py", "exec")
-    #os.system("python3 bit_plane_slicing_gui.py")
+    path = os.path.join(os.getcwd(), "bitplane/bit_plane_slicing_gui.py")
+
 elif args.command=="bph":
-    with open("bitplane\\bit_plane_hiding_gui.py", "rb") as source_file:
-        code = compile(source_file.read(), "bitplane\\bit_plane_hiding_gui.py", "exec")
+    path = os.path.join(os.getcwd(), "bitplane/bit_plane_hiding_gui.py")
 
 elif args.command=="lsbh":
-    with open("lsb\\lsb_steg_hiding_gui.py", "rb") as source_file:
-        code = compile(source_file.read(), "lsb\\lsb_steg_hiding_gui.py", "exec")
+    path = os.path.join(os.getcwd(), "lsb/lsb_steg_hiding_gui.py")
 
 elif args.command=="lsbu":
-    with open("lsb\\lsb_steg_gui.py", "rb") as source_file:
-        code = compile(source_file.read(), "lsb\\lsb_steg_gui.py", "exec")
+    path = os.path.join(os.getcwd(), "lsb/lsb_steg_gui.py")
 
 elif args.command=="triplea":
-    with open("tripleA\\triple_A_gui.py", "rb") as source_file:
-        code = compile(source_file.read(), "triplea\\triple_A_gui.py.py", "exec")
-    #os.system("python3 triple_A_gui.py")
-else:
-    code = ""
+    path = os.path.join(os.getcwd(), "triplea/triple_A_gui.py")
 
-if code != "":
+else:
+    path = ""
+
+if path != "":
+    with open(path, "rb") as source_file:
+        code = compile(source_file.read(), path, "exec")
     exec(code)
